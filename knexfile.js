5a92acdb0c1cd5d6806147ab03620f332180a85b
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-10-09 14:44:17 -0700

    Fixed Minor error regarding seeds in knexfile

--- knexfile.js
+++ knexfile.js
@@ -33,7 +33,7 @@ module.exports = {
         directory: __dirname + '/db/migrations',
     },
     seeds: {
-        directory: __dirname + '/db/seeds/production',
+        directory: __dirname + '/db/seeds',
     }
   }
 
