c200f6a4fd1897774a468936e38eebaefe6a3c4b
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-10-09 14:27:06 -0700

    Updated Database.js

--- db/Database.js
+++ db/Database.js
@@ -6,20 +6,20 @@ const database = require('knex')(config[process.env.NODE_ENV] || config['develop
 
 // On API startup, this will run knex migrate:latest to ensure that our DB is up to date.
 // const knexSetup = async () => {
-//   if (process.env.NODE_ENV != 'development') {
-//     console.log('>>> Production Environment Detected: Running `knex migrate:latest`.')
-//     await database.migrate.latest('production')
-//     console.log('>>> Migrations Complete.')
-//     await database.seed.run('production')
-//     console.log('>>> Seed Complete.')
-//   } else if (process.env.NODE_ENV == 'development') {
-//     console.log('>>> Development Environment Detected: Running `database migrate:latest`.')
-//     await database.migrate.latest('development')
-//     console.log('>>> Migrations Complete.')
-//     await database.seed.run('production')
-//     console.log('>>> Seed Complete.')
-//   }
-// }
-// knexSetup();
+  if (process.env.NODE_ENV != 'development') {
+    console.log('>>> Production Environment Detected: Running `knex migrate:latest`.')
+    await database.migrate.latest('production')
+    console.log('>>> Migrations Complete.')
+    await database.seed.run('production')
+    console.log('>>> Seed Complete.')
+  } else if (process.env.NODE_ENV == 'development') {
+    console.log('>>> Development Environment Detected: Running `database migrate:latest`.')
+    await database.migrate.latest('development')
+    console.log('>>> Migrations Complete.')
+    await database.seed.run('production')
+    console.log('>>> Seed Complete.')
+  }
+}
+knexSetup();
 
 module.exports = database;
